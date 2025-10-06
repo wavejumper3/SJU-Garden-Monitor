305649e535054971200905a8de4baf2bb7af22b9
Albert Adamson <albertjj206@gmail.com>
2025-10-05 23:14:19 -0400

    Changed install script

--- INSTALL.sh
+++ INSTALL.sh
@@ -1,10 +1,20 @@
 #!/usr/bin/env bash
+set -e  # exit on any errors
 
-venv_name=SJU-GARDEN-MONITOR
+venv_name="SJU-GARDEN-MONITOR"
+
+# Check if the virtual environment exists
+if [ -d "$venv_name" ]; then
+    echo "Virtual environment '$venv_name' already exists. Exiting."
+    exit 1
+fi
 
 # Create the python virtual environment
-python -m venv $venv_name
-source $venv_name/bin/activate
+python3 -m venv "$venv_name"
+source "$venv_name/bin/activate"
+"$venv_name/bin/pip" install --upgrade pip
 
 # Install packages to the virtual environment
-pip install -r requirements.txt
+"$venv_name/bin/pip" install -r requirements.txt
+
+echo "Successfully created virtual environment"
